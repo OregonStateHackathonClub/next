e640225c776962c7412a15f14995b507fa610397
Marko Al-Hamadani <markoalhamadani@gmail.com>
2025-07-28 14:32:44 -0400

    fixed HTML tag errors

--- src/components/application/StepTwo.tsx
+++ src/components/application/StepTwo.tsx
@@ -9,15 +9,15 @@ export const StepTwo = () => {
       <form className="text-white-600 mb-8 pl-8 text-left">
         <div>
           <label
-            for="projectName"
-            class="block mb-2 text-white-3xl font-bold dark:text-white"
+            htmlFor="projectName"
+            className="block mb-2 text-white-3xl font-bold dark:text-white"
           >
             Project Title
           </label>
           <input
             type="text"
             id="projectTitle"
-            class="block w-half p-2 text-gray-600 border border-gray-300 rounded-lg bg-gray-50
+            className="block w-half p-2 text-gray-600 border border-gray-300 rounded-lg bg-gray-50
             focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Project Title..."
           />
@@ -25,15 +25,15 @@ export const StepTwo = () => {
         <br />
         <div>
           <label
-            for="projectDescription"
-            class="block mb-2 text-white-3xl font-bold dark:text-white"
+            htmlFor="projectDescription"
+            className="block mb-2 text-white-3xl font-bold dark:text-white"
           >
             Project Desciption
           </label>
           <input
             type="text"
             id="projectDescription"
-            class="block w-full p-4 text-gray-600 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-4 text-gray-600 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Project Description..."
           />
         </div>
